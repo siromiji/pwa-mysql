9fd1811046313ef06ec23767b1451704ca3d2697
siromiji <mnmn82018282@gmail.com>
2025-10-30 17:23:33 +0900

    251030 ~SELECT문

---
+++ edu/01_select.sql
@@ -0,0 +1,32 @@
+-- 테이블 전체 컬럼 조회
+SELECT *
+FROM employees;
+
+-- 특정 컬럼만 지정하여 조회
+SELECT 
+	name
+	,birth
+	,hire_at
+FROM employees;
+
+-- WHERE 절 :특정 컬럼의 값과 일치한 데이터만 조회
+SELECT *
+FROM employees
+WHERE
+	emp_id = 5
+;
+
+SELECT *
+FROM employees
+WHERE
+	NAME = '강영화'
+
+;
+
+SELECT *
+FROM employees
+WHERE
+	NAME = '강영화' AND	GENDER = 'M'
+
+;
+	
